340840c16ddd0af752f6c7a07f57fe340b0f2487
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-06-28 14:59:07 +0200

    patch: rename file.cpp → main.cpp

--- simon/fasta-benchmarks/build.sh
+++ simon/fasta-benchmarks/build.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 g++ -std=c++20 -march=native -O3 -DNDEBUG -lz\
-    file.cpp seqan2.cpp seqan3.cpp \
+    main.cpp seqan2.cpp seqan3.cpp \
     -DSEQAN_HAS_ZLIB  -I../../lib/seqan/include \
     -DSEQAN3_HAS_ZLIB -I../../lib/seqan3/include -I../../lib/seqan3/submodules/sdsl-lite/include
 
