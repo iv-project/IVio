123d6665fe05c969a95450523ff6134e3d647bd0
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-06-28 14:59:45 +0200

    patch: rename a.out → benchmark

--- simon/fasta-benchmarks/bench.sh
+++ simon/fasta-benchmarks/bench.sh
@@ -10,7 +10,7 @@ for file in ${files[@]}; do
         #echo "  method: $method"
         printf "%s %s" $file $method
         for i in $(seq 3); do
-            /usr/bin/time -f "run %e %M" ./a.out ${method} ${file} 2>&1 | grep run
+            /usr/bin/time -f "run %e %M" ./benchmark ${method} ${file} 2>&1 | grep run
         done | cut -b 4- | sort -n | head -n 1
     done
 done
--- simon/fasta-benchmarks/build.sh
+++ simon/fasta-benchmarks/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
 
-g++ -std=c++20 -march=native -O3 -DNDEBUG -lz\
+g++ -std=c++20 -march=native -O3 -DNDEBUG -lz -o benchmark\
     main.cpp seqan2.cpp seqan3.cpp \
     -DSEQAN_HAS_ZLIB  -I../../lib/seqan/include \
     -DSEQAN3_HAS_ZLIB -I../../lib/seqan3/include -I../../lib/seqan3/submodules/sdsl-lite/include
