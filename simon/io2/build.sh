3e892b5a0e3c76b63ef824a2c787bb73b24b0a4d
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2022-06-30 16:57:40 +0200

    move io2 outside of the fasta-benchmarks

--- simon/io2/build.sh
+++ simon/io2/build.sh
@@ -4,9 +4,8 @@ cd "$(dirname "$0")"
 FLAGS="-march=native -O3 -DNDBEUG"
 #FLAGS="-O0 -ggdb"
 
-g++ -std=c++20 ${FLAGS} -lz -o benchmark \
-    main.cpp seqan2.cpp seqan3.cpp bio.cpp \
-    -I ../io2/include \
+g++ -std=c++20 ${FLAGS} -lz -o io2 \
+    io2_demo.cpp \
+    -I include \
     -DSEQAN_HAS_ZLIB  -I../../lib/seqan/include \
-    -DBIO_HAS_ZLIB -I../../lib/b.i.o./include \
     -DSEQAN3_HAS_ZLIB -I../../lib/seqan3/include -I../../lib/seqan3/submodules/sdsl-lite/include
