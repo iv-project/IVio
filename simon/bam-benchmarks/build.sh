297190f24a4ed536efb68b52ce887875abdd7073
smehringer <svenja.mehringer@gmail.com>
2022-11-17 14:24:46 +0100

    [INFRA] Adapt simons build.sh

--- simon/bam-benchmarks/build.sh
+++ simon/bam-benchmarks/build.sh
@@ -2,10 +2,12 @@
 cd "$(dirname "$0")"
 
 FLAGS="-march=native -O3 -DNDEBUG"
+#FLAGS="-march=native -O3 -DNDEBUG -g -fno-omit-frame-pointer -ggdb -g3"
 #FLAGS="-O0 -ggdb"
 
-g++ -std=c++20 ${FLAGS} -lz -o benchmark \
+g++-11 -std=c++20 ${FLAGS} -lz -o benchmark \
     main.cpp seqan3.cpp seqan2.cpp io2.cpp \
+    -lpthread \
     -I../../io2/include \
     -DSEQAN_HAS_ZLIB  -I../../lib/seqan/include \
     -DSEQAN_BGZF_NUM_THREADS=4 \
