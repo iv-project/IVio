8d31a32b0bcd5bde5237c308c700763823c29f8e
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-01-25 18:00:14 +0100

    patch: remove all unused headers from direct.cpp

--- simon/fasta-benchmarks/src/direct.cpp
+++ simon/fasta-benchmarks/src/direct.cpp
@@ -1,27 +1,8 @@
-#include <cassert>
-#include <cstring>
-#include <cstdint>
-#include <unistd.h>
 #include <array>
-#include <algorithm>
-#include <optional>
-#include <vector>
-#include <limits>
-#include <numeric>
-#include <variant>
+#include <io3/mmap_reader.h>
 #include <iostream>
-#include <fstream>
 
-#include "file_reader.h"
-
-#include "mmap_file_reader.h"
-#include "zlib_reader.h"
-#include "zlib_mmap_reader.h"
-
-#include "io3/fasta/reader.h"
-#include "io3/fasta/writer.h"
-
-constexpr static std::array<uint8_t, 256> ccmap = []() {
+constexpr static auto ccmap = []() {
     std::array<uint8_t, 256> c;
     c.fill(3);
     c['A'] = 0;
@@ -42,22 +23,8 @@ constexpr static std::array<uint8_t, 256> ccmap = []() {
     return c;
 }();
 
-#include "fasta_reader_view.h"
-#include "fasta_reader_contigous.h"
-#include "fasta_reader_mmap.h"
-#include "fasta_reader_mmap2.h"
-//#include "fasta_reader_best.h"
-
-#include <io3/mmap_reader.h>
-
-#include <ranges>
-#include <iostream>
-
-
-
 void direct_bench(std::filesystem::path path) {
     auto reader = io3::mmap_reader(path.c_str());
-
 
     std::array<int, 256> ctChars{};
 
