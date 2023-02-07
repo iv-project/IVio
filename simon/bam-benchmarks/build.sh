d39b1d59105aecdedc808497184cf879745a9b7c
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-02-07 11:28:42 +0100

    patch: reworking bam-benchmark, so the folder structure is the same as the other benchmarks

--- simon/bam-benchmarks/build.sh
+++ simon/bam-benchmarks/build.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
 
-FLAGS="-march=native -O3 -DNDEBUG"
-#FLAGS="-march=native -O3 -DNDEBUG -g -fno-omit-frame-pointer -ggdb -g3"
-#FLAGS="-O0 -ggdb"
+FLAGS="-march=native -O3 -DNDEBUG -s -Wall"
+#FLAGS="-O0 -ggdb -Wall"
+#FLAGS="-O0 -ggdb -fsanitize=address -Wall"
+#FLAGS="-march=native -O3 -DNDEBUG -ggdb"
+#
+INCLUDES="-I ../../io2/include \
+    -I ../ \
+    -DSEQAN_HAS_ZLIB  -isystem../../lib/seqan/include \
+    -DBIO_HAS_ZLIB -isystem../../lib/b.i.o./include \
+    -DSEQAN3_HAS_ZLIB -isystem../../lib/seqan3/include -isystem../../lib/submodules/sdsl-lite/include"
+ARGS="-std=c++20 ${FLAGS} ${INCLUDES}"
 
-g++-11 -std=c++20 ${FLAGS} -lz -o benchmark \
-    main.cpp seqan3.cpp seqan2.cpp io2.cpp \
-    -lpthread \
-    -I../../io2/include \
-    -DSEQAN_HAS_ZLIB  -I../../lib/seqan/include \
-    -DSEQAN_BGZF_NUM_THREADS=4 \
-    -DSEQAN3_HAS_ZLIB -I../../lib/seqan3/include -I../../lib/submodules/sdsl-lite/include
+
+mkdir -p obj/io3/fastq obj/io3/bcf obj/io3/vcf obj/io3/bam obj/src
+g++ ${ARGS} -c ../io3/fastq/reader.cpp -o obj/io3/fastq/reader.o
+g++ ${ARGS} -c src/main.cpp -o obj/src/main.o
+g++ ${ARGS} -c src/seqan2.cpp -o obj/src/seqan2.o
+g++ ${ARGS} -c src/seqan3.cpp -o obj/src/seqan3.o
+g++ ${ARGS} -c src/io2.cpp -o obj/src/io2.o
+
+g++ $(find obj | grep \.o\$) -lz-ng -lz ${FLAGS} -o benchmark
