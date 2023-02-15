3d4bd8bba03940aac9ee85b162f0a595985f8ae2
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-02-15 16:48:40 +0100

    Infra: moving io3 to the root folder
    moving io3 to the root folder, removing everything that is not
    interessting. fixing the build.sh pointing to the new paths.

---
+++ benchmarks/bam-benchmarks/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+cd "$(dirname "$0")"
+
+FLAGS="-march=native -O3 -DNDEBUG -s -Wall"
+#FLAGS="-O0 -ggdb -Wall"
+#FLAGS="-O0 -ggdb -fsanitize=address -Wall"
+#FLAGS="-march=native -O3 -DNDEBUG -ggdb"
+
+INCLUDES="-I ../../src/ \
+    -isystem../../lib/io2/include \
+    -DSEQAN_HAS_ZLIB  -isystem../../lib/seqan/include \
+    -DBIO_HAS_ZLIB -isystem../../lib/b.i.o./include \
+    -DSEQAN3_HAS_ZLIB -isystem../../lib/seqan3/include -isystem../../lib/submodules/sdsl-lite/include"
+ARGS="-std=c++20 ${FLAGS} ${INCLUDES}"
+
+# Building io3 library
+for f in $(find ../../src/io3 | grep .cpp\$); do
+    g=$(echo $f | cut -b 11-)
+    mkdir -p $(dirname obj/$g)
+    g++ ${ARGS} -c $f -o obj/$g.o
+done
+
+# Building benchmark
+for f in $(find src | grep .cpp\$); do
+    mkdir -p $(dirname obj/$f)
+    g++ ${ARGS} -c $f -o obj/$f.o
+done
+g++ $(find obj/src | grep \.o\$) $(find obj/io3 | grep \.o\$) -lz-ng -lz ${FLAGS} -o benchmark
--- benchmarks/fasta-benchmarks/build.sh
+++ benchmarks/fasta-benchmarks/build.sh
@@ -7,32 +7,36 @@ FLAGS="-march=native -O3 -DNDEBUG -s -fconcepts-diagnostics-depth=10 -Wall"
 #FLAGS="-O0 -ggdb -Wall"
 #FLAGS="-O0 -ggdb -fsanitize=address -Wall"
 #FLAGS="-march=native -O3 -DNDEBUG -ggdb"
-#
-INCLUDES="-I ../../io2/include \
-    -I ../ \
+
+INCLUDES="-I ../../src/ \
+    -isystem../../lib/io2/include \
     -DSEQAN_HAS_ZLIB  -isystem../../lib/seqan/include \
     -DBIO_HAS_ZLIB -isystem../../lib/b.i.o./include \
     -DSEQAN3_HAS_ZLIB -isystem../../lib/seqan3/include -isystem../../lib/submodules/sdsl-lite/include"
 ARGS="-std=c++20 ${FLAGS} ${INCLUDES}"
 
+# Building io3 library
+for f in $(find ../../src/io3 | grep .cpp\$); do
+    g=$(echo $f | cut -b 11-)
+    mkdir -p $(dirname obj/$g)
+    g++ ${ARGS} -c $f -o obj/$g.o
+done
 
-mkdir -p obj/io3/fasta obj/io3/bcf obj/io3/vcf
-g++ ${ARGS} -c ../io3/fasta/reader.cpp -o obj/io3/fasta/reader.o
-g++ ${ARGS} -c ../io3/fasta/reader_mt.cpp -o obj/io3/fasta/reader_mt.o
-g++ ${ARGS} -c ../io3/fasta/writer.cpp -o obj/io3/fasta/writer.o
-
+# Building benchmark_read
 for f in $(find src/read | grep .cpp\$); do
     mkdir -p $(dirname obj/$f)
     g++ ${ARGS} -c $f -o obj/$f.o
 done
 g++ $(find obj/src/read | grep \.o\$) $(find obj/io3 | grep \.o\$) -lz-ng -lz ${FLAGS} -o benchmark_read
 
+# Building benchmark_read_stream
 for f in $(find src/read_stream | grep .cpp\$); do
     mkdir -p $(dirname obj/$f)
     g++ ${ARGS} -c $f -o obj/$f.o
 done
 g++ $(find obj/src/read_stream | grep \.o\$) $(find obj/io3 | grep \.o\$) -lz-ng -lz ${FLAGS} -o benchmark_read_stream
 
+# Building benchmark_write
 for f in $(find src/write | grep .cpp\$); do
     mkdir -p $(dirname obj/$f)
     g++ ${ARGS} -c $f -o obj/$f.o
