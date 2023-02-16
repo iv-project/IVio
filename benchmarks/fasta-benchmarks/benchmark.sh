0d0e690b87dd9d57234c20ab6d46eb06aaaad003
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-02-16 17:09:38 +0100

    patch: first io2 then bio

--- benchmarks/fasta-benchmarks/benchmark.sh
+++ benchmarks/fasta-benchmarks/benchmark.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 
 type=$1
 
-methods=(seqan2 seqan3 bio io2 ivio ivio_mt direct extreme)
+methods=(seqan2 seqan3 io2 bio ivio ivio_mt direct extreme)
 files=(../data/illumina.fa ../data/illumina.fa.gz ../data/hg38.fa ../data/hg38.fa.gz)
 if [ "${type}" == "write" ]; then
     files=(../data/illumina.fa ../data/hg38.fa)
