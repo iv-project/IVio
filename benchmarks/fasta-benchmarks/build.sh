9b8f638bf594009b0003ec7aeda70739fc6c9287
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-02-20 09:43:37 +0100

    patch: unifying all build scripts into a single script inside of utils

--- benchmarks/fasta-benchmarks/build.sh
+++ benchmarks/fasta-benchmarks/build.sh
@@ -1,43 +1,8 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
 
-FLAGS="-march=native -O3 -DNDEBUG -s -fconcepts-diagnostics-depth=10 -Wall"
-#FLAGS="-O0 -ggdb -Wall"
-#FLAGS="-O0 -ggdb -fsanitize=address -Wall"
-#FLAGS="-march=native -O3 -DNDEBUG -ggdb"
+source ../utils/build.sh
 
-INCLUDES="-I . -I ../../src/ \
-    -isystem../../lib/io2/include \
-    -DSEQAN_HAS_ZLIB  -isystem../../lib/seqan/include \
-    -DBIO_HAS_ZLIB -isystem../../lib/biocpp-io/include \
-                   -isystem../../lib/biocpp-core/include \
-    -DSEQAN3_HAS_ZLIB -isystem../../lib/seqan3/include -isystem../../lib/submodules/sdsl-lite/include"
-ARGS="-std=c++20 ${FLAGS} ${INCLUDES}"
-
-# Building ivio library
-for f in $(find ../../src/ivio | grep .cpp\$); do
-    g=$(echo $f | cut -b 11-)
-    mkdir -p $(dirname obj/$g)
-    g++ ${ARGS} -c $f -o obj/$g.o
-done
-
-# Building benchmark_read
-for f in $(find src/read | grep .cpp\$); do
-    mkdir -p $(dirname obj/$f)
-    g++ ${ARGS} -c $f -o obj/$f.o
-done
-g++ $(find obj/src/read | grep \\\.o\$) $(find obj/ivio | grep \\\.o\$) -lz-ng -lz ${FLAGS} -o benchmark_read
-
-# Building benchmark_read_stream
-for f in $(find src/read_stream | grep .cpp\$); do
-    mkdir -p $(dirname obj/$f)
-    g++ ${ARGS} -c $f -o obj/$f.o
-done
-g++ $(find obj/src/read_stream | grep \\\.o\$) $(find obj/ivio | grep \\\.o\$) -lz-ng -lz ${FLAGS} -o benchmark_read_stream
-
-# Building benchmark_write
-for f in $(find src/write | grep .cpp\$); do
-    mkdir -p $(dirname obj/$f)
-    g++ ${ARGS} -c $f -o obj/$f.o
-done
-g++ $(find obj/src/write | grep \\\.o\$) $(find obj/ivio | grep \\\.o\$) -lz-ng -lz ${FLAGS} -o benchmark_write
+build src/read benchmark_read
+build src/read_stream benchmark_read_stream
+build src/write benchmark_write
---
+++ benchmarks/utils/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+type="$1"
+
+if [ -z "${type}" ]; then
+    type="release"
+fi
+
+
+if [ "${type}" == "release" ]; then
+    FLAGS="-march=native -O3 -DNDEBUG -s -fconcepts-diagnostics-depth=10 -Wall "
+elif [ "${type}" == "debug" ]; then
+    FLAGS="-O0 -ggdb -Wall"
+fi
+#FLAGS="-O0 -ggdb -fsanitize=address -Wall"
+#FLAGS="-march=native -O3 -DNDEBUG -ggdb"
+
+
+INCLUDES="-I . -I ../../src/ \
+    -isystem../../lib/io2/include \
+    -DSEQAN_HAS_ZLIB  -isystem../../lib/seqan/include \
+    -DBIO_HAS_ZLIB -isystem../../lib/biocpp-io/include \
+                   -isystem../../lib/biocpp-core/include \
+    -DSEQAN3_HAS_ZLIB -isystem../../lib/seqan3/include -isystem../../lib/submodules/sdsl-lite/include"
+ARGS="-std=c++20 ${FLAGS} ${INCLUDES}"
+
+
+# Building ivio library
+for f in $(find ../../src/ivio | grep .cpp\$); do
+    g=$(echo $f | cut -b 11-)
+    mkdir -p $(dirname obj/$g)
+    g++ ${ARGS} -c $f -o obj/$g.o
+done
+
+build () {
+    folder=$1
+    target=$2
+    # Building benchmark
+    for f in $(find ${folder} | grep .cpp\$); do
+        mkdir -p $(dirname obj/$f)
+        g++ ${ARGS} -c $f -o obj/$f.o
+    done
+    g++ $(find obj/${folder} | grep \\\.o\$) $(find obj/ivio | grep \\\.o\$) -lz-ng -lz ${FLAGS} -o ${target}
+}
