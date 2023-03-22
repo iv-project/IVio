333daf54f52cc745c6a49b36c339137d6d48780a
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-03-22 20:23:45 +0100

    patch: restructure sam record such that it similar to vcf and fasta

--- src/ivio/sam/record.h
+++ src/ivio/sam/record.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <cstddef>
+#include <string>
 #include <string_view>
 
 namespace ivio::sam {
+
+struct record;
 
 struct record_view {
     std::string_view            qname;
@@ -17,6 +20,9 @@ struct record_view {
     int32_t                     tlen;
     std::string_view            seq;
     std::string_view            qual;
+
+    operator record() const;
+    auto operator<=>(record_view const&) const = default;
 };
 
 struct record {
@@ -32,35 +38,51 @@ struct record {
     std::string            seq;
     std::string            qual;
 
-    record() = default;
-    record(record_view v)
-        : qname{v.qname}
-        , flag {v.flag}
-        , rname{v.rname}
-        , pos  {v.pos}
-        , mapq {v.mapq}
-        , cigar{v.cigar}
-        , rnext{v.rnext}
-        , pnext{v.pnext}
-        , tlen {v.tlen}
-        , seq  {v.seq}
-        , qual {v.qual}
-    {}
-    operator record_view() const {
-        return record_view {
-            qname,
-            flag,
-            rname,
-            pos,
-            mapq,
-            cigar,
-            rnext,
-            pnext,
-            tlen,
-            seq,
-            qual
-        };
-    }
+    operator record_view() const&;
+    auto operator<=>(record const&) const = default;
 };
 
+// Implementation of the convert operators
+inline record_view::operator record() const {
+    return {
+        .qname   = std::string{qname},
+        .flag    = flag,
+        .rname   = std::string{rname},
+        .pos     = pos,
+        .mapq    = mapq,
+        .cigar   = std::string{cigar},
+        .rnext   = std::string{rnext},
+        .pnext   = pnext,
+        .tlen    = tlen,
+        .seq     = std::string{seq},
+        .qual    = std::string{qual},
+    };
 }
+inline record::operator record_view() const& {
+    return {
+        .qname   = qname,
+        .flag    = flag,
+        .rname   = rname,
+        .pos     = pos,
+        .mapq    = mapq,
+        .cigar   = cigar,
+        .rnext   = rnext,
+        .pnext   = pnext,
+        .tlen    = tlen,
+        .seq     = seq,
+        .qual    = qual,
+    };
+}
+
+}
+
+// Specialization to describe their common types
+template <>
+struct std::common_type<ivio::sam::record, ivio::sam::record_view> {
+    using type = ivio::sam::record;
+};
+
+template <>
+struct std::common_type<ivio::sam::record_view, ivio::sam::record> {
+    using type = ivio::sam::record;
+};
