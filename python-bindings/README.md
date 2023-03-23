a473b760cdbb0f18dd6c92dc61c2ba3e9b1803e1
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-03-23 10:51:23 +0100

    fix: removing python bindings

--- python-bindings/README.md
+++
@@ -1,9 +0,0 @@
-# iviopy
-
-This folder helps creating libraries for python.
-
-
-Just run `./build.sh`
-
-
-And afterwards `twine upload --repository testpypi wheelhouse/*.whl --verbose`
--- python-bindings/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-cd "$(dirname "$0")"
-
-docker run -u $(id -u) -ti --rm -e PLAT="manylinux_2_28_x86_64" -v $(pwd)/..:/code quay.io/pypa/manylinux_2_28_x86_64 /code/python-bindings/build-wheels.sh
