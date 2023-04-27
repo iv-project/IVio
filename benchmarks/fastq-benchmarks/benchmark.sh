d5b1e5e7570971a661ae799aa9922981de503c80
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-04-27 20:55:22 +0200

    fix: adding license text to each file

--- benchmarks/fastq-benchmarks/benchmark.sh
+++ benchmarks/fastq-benchmarks/benchmark.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/bash
+# -----------------------------------------------------------------------------------------------------
+# Copyright (c) 2006-2023, Knut Reinert & Freie Universität Berlin
+# Copyright (c) 2016-2023, Knut Reinert & MPI für molekulare Genetik
+# This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+# shipped with this file.
+# -----------------------------------------------------------------------------------------------------
 cd "$(dirname "$0")"
 
 methods=(seqan2 seqan3 bio ivio)
