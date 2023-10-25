a8455e2249dc86d2f0c6450cad9acf40670be370
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-10-25 13:27:37 +0200

    patch: missing fixes going from catch v2 to v3

--- src/test_ivio/main.cpp
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: 2006-2023, Knut Reinert & Freie Universität Berlin
-// SPDX-FileCopyrightText: 2016-2023, Knut Reinert & MPI für molekulare Genetik
-// SPDX-License-Identifier: BSD-3-Clause
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
